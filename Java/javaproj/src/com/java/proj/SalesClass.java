241399857379a7cb08ae877957b5bd2073a461be
pvnkamesh <57649450+pvnkamesh@users.noreply.github.com>
2019-11-11 21:29:36 -0500

    Saleclass java class draft version

---
+++ Java/javaproj/src/com/java/proj/SalesClass.java
@@ -0,0 +1,12 @@
+package com.java.proj;
+
+public class SalesClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Sales Class");
+
+	}
+
+}
